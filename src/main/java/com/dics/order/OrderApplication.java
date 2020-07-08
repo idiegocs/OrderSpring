9adb837574f2a098357dbe575265d7ecf828baaa
idiegocs <idiegocs@gmail.com>
2020-07-08 09:05:06 -0500

    se crean variables de entorno

--- src/main/java/com/dics/order/OrderApplication.java
+++ src/main/java/com/dics/order/OrderApplication.java
@@ -3,7 +3,6 @@ package com.dics.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.jms.annotation.EnableJms;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,7 +10,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableJms
 @SpringBootApplication(exclude = JmxAutoConfiguration.class)
-@ImportResource("classpath:/service-integration-config.xml")
 public class OrderApplication {
 
 	public static void main(String[] args) {
